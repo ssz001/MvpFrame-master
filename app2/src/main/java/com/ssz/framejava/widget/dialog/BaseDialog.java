52839317223307d4ff8e705c8bd75e30af100627
zsp <906830124@qq.com>
2020-01-09 16:07:56 +0800

    RxSchedules

--- app2/src/main/java/com/ssz/framejava/widget/dialog/BaseDialog.java
+++ app2/src/main/java/com/ssz/framejava/widget/dialog/BaseDialog.java
@@ -2,45 +2,110 @@ package com.ssz.framejava.widget.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.ssz.framejava.base.app.helper.AppHelper;
+import com.ssz.framejava.utils.ObjectHelper;
+import com.ssz.framejava.utils.toast.ToastUtil;
+
+import org.jetbrains.annotations.NotNull;
+
+import butterknife.ButterKnife;
 
 /**
  * @author : zsp
  * time : 2019 12 2019/12/30 16:19
  */
-public abstract class BaseDialog extends Dialog implements View.OnClickListener {
+public abstract class BaseDialog extends Dialog {
 
-    private View mView;
+    public final int WRAP_CONTENT = WindowManager.LayoutParams.WRAP_CONTENT;
+
+    protected View rootView;
+    private DialogConfig mConfig;
 
     public BaseDialog(@NonNull Context context) {
         super(context);
+        init(context);
     }
 
     public BaseDialog(@NonNull Context context, int themeResId) {
         super(context, themeResId);
+        init(context);
     }
 
-    protected BaseDialog(@NonNull Context context, boolean cancelable, @Nullable DialogInterface.OnCancelListener cancelListener) {
-        super(context, cancelable, cancelListener);
+    private void init(Context context){
+        mConfig = ObjectHelper.requireNotNull(loadConfig(), "config == null");
+        rootView = LayoutInflater.from(context).inflate(mConfig.viewId, null);
+        ButterKnife.bind(this, rootView);
+        applyRootView(rootView);
     }
 
-    protected abstract int getLayoutId();
+    @NotNull
+    public abstract DialogConfig loadConfig();
+
+    public abstract void applyRootView(View rootView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getLayoutId());
+        setContentView(rootView);
+        // setCanceledOnTouchOutside 优先级高
+        setCancelable(mConfig.cancelAble);
+        setCanceledOnTouchOutside(mConfig.canceledOnTouchOutside);
+
+        Window win = getWindow();
+        WindowManager.LayoutParams lp;
+        if (win != null) {
+            lp = win.getAttributes();
+            lp.gravity = mConfig.gravity;
+            lp.height = mConfig.height;
+            lp.width = mConfig.width;
+            win.setAttributes(lp);
+        }
     }
 
-    static class Config{
-        private Context context;
-        private int height, width;
-        private boolean outsideCancelAble;
-        private View view;
-        private int resStyle = -1;
+    public int unit(int size, int typeValue) {
+        return (int) (TypedValue.applyDimension(typeValue, size,
+                AppHelper.getApplication().getResources().getDisplayMetrics()));
+    }
+
+    /*************************** get resources *************************/
+
+    public int getColorById(int res){
+        return ContextCompat.getColor(getContext(),res);
+    }
+
+    public Drawable getDrawableById(int res){
+        return ContextCompat.getDrawable(getContext(),res);
+    }
+
+    public int getDimenById(int dimenId){
+        return (int)getContext().getResources().getDimension(dimenId);
+    }
+
+    public float getDimenFloatById(int dimenId){
+        return getContext().getResources().getDimension(dimenId);
+    }
+
+    public String getStringById(int resId){
+        return getContext().getResources().getString(resId);
+    }
+
+    /*************************** get resources end *************************/
+
+    protected void showToast(final String msg) {
+        ToastUtil.showToast(getContext(), msg);
+    }
+
+    protected void showToast(final String msg, int gravity) {
+        ToastUtil.showToast(getContext(), msg, gravity);
     }
 }
